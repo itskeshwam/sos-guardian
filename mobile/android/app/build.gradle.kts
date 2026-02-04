40a9f076b007580d86d544b5d210acdc92a71bf8
Keshwam Pandey <pandeykeshwam1122@gmail.com>
2026-02-04 12:27:06 +0530

    Fixed Gradle Error

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -25,7 +24,7 @@ android {
         }
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 
     sourceSets {
@@ -35,8 +34,6 @@ android {
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.mobile"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://docs.flutter.dev/deployment/android#reviewing-the-gradle-build-configuration.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -45,8 +42,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
--- mobile/android/settings.gradle.kts
+++ mobile/android/settings.gradle.kts
@@ -18,9 +18,34 @@ pluginManagement {
 }
 
 plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.9.1" apply false
+    id("com.android.application") version "8.13.2" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
 }
 
 include(":app")
+
+fun includeFlutterPlugins(settings: Settings) {
+    val flutterProjectRoot = settings.rootProject.projectDir.parentFile
+    val pluginsFile = File(flutterProjectRoot, ".flutter-plugins-dependencies")
+    if (pluginsFile.exists()) {
+        val properties = java.util.Properties()
+        pluginsFile.inputStream().use { properties.load(it) }
+        val plugins = properties.getProperty("plugins.flutter.plugin_packages", "")
+        plugins.split(',').forEach { pluginName ->
+            val plugin = pluginName.trim()
+            if (plugin.isNotEmpty()) {
+                val pluginPath = properties.getProperty("$plugin.path")
+                if (pluginPath != null) {
+                    val pluginDir = File(pluginPath)
+                    settings.includeBuild(pluginDir) {
+                        dependencySubstitution {
+                            substitute(module("io.flutter.plugins.${plugin.replace("_", "")}:flutter")).using(project(":"))
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+includeFlutterPlugins(settings)
