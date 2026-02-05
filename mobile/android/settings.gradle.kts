bb9d77fd169aae7955f35e72015716c53e2f2fa9
Keshwam Pandey <pandeykeshwam1122@gmail.com>
2026-02-05 12:34:14 +0530

    Build Issue

--- mobile/android/settings.gradle.kts
+++ mobile/android/settings.gradle.kts
@@ -1,14 +1,13 @@
+val properties = java.util.Properties()
+val propertiesFile = file("local.properties")
+if (propertiesFile.exists()) {
+    propertiesFile.inputStream().use { properties.load(it) }
+}
+val flutterSdkPath = properties.getProperty("flutter.sdk")
+require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
+
 pluginManagement {
-    val flutterSdkPath by lazy {
-        val properties = java.util.Properties()
-        file(settingsDir, "local.properties").inputStream().use { properties.load(it) }
-        val flutterSdkPath = properties.getProperty("flutter.sdk")
-        require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
-        flutterSdkPath
-    }
-
-    includeBuild(File(flutterSdkPath, "packages/flutter_tools/gradle"))
-
+    includeBuild(java.io.File(flutterSdkPath, "packages/flutter_tools/gradle"))
     repositories {
         google()
         mavenCentral()
@@ -23,29 +22,3 @@ plugins {
 }
 
 include(":app")
-
-fun includeFlutterPlugins(settings: Settings) {
-    val flutterProjectRoot = settings.rootProject.projectDir.parentFile
-    val pluginsFile = File(flutterProjectRoot, ".flutter-plugins-dependencies")
-    if (pluginsFile.exists()) {
-        val properties = java.util.Properties()
-        pluginsFile.inputStream().use { properties.load(it) }
-        val plugins = properties.getProperty("plugins.flutter.plugin_packages", "")
-        plugins.split(',').forEach { pluginName ->
-            val plugin = pluginName.trim()
-            if (plugin.isNotEmpty()) {
-                val pluginPath = properties.getProperty("$plugin.path")
-                if (pluginPath != null) {
-                    val pluginDir = File(pluginPath)
-                    settings.includeBuild(pluginDir) {
-                        dependencySubstitution {
-                            substitute(module("io.flutter.plugins.${plugin.replace("_", "")}:flutter")).using(project(":"))
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
-includeFlutterPlugins(settings)
